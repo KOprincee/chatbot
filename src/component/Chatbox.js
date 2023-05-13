40e0547da2cae77c608586fac0a89b79ee723e06
Prince Kothari <princemkothari@gmail.com>
2023-05-13 22:36:13 +0530

    UI and Logic Changes

--- src/component/Chatbox.js
+++ src/component/Chatbox.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+//import ChatBotImg from "../assets/chatbot.png";
+import GuestImg from "../assets/guest.png";
+import "./Chatbox.css";
+
 const ChatBox = () => {
+  const [userInput, setuserInput] = useState("");
+  const [userMsg, setUserMsg] = useState([]);
+
+  const inputSubmitHandler = (e) => {
+    //Preventing reloading of the form
+    e.preventDefault();
+
+    //Creating Message List
+    setUserMsg((prevMsg) => [
+      ...prevMsg,
+      {
+        from: "user",
+        msg: userInput,
+      },
+    ]);
+
+    //Resetting input tag
+    setuserInput("");
+  };
+
   return (
     <div className="chatbox-container">
-      <div className="chatbox-messgaes"></div>
+      <div className="chatbox-messgaes">
+        <ul id="messages">
+          {userMsg.map((data, index) => (
+            <li key={index} className="chat-msg">
+              <img src={GuestImg} alt="Guest"></img>
+              <p>{data.msg}</p>
+            </li>
+          ))}
+        </ul>
+        <form className="ChatBot-Input" onSubmit={inputSubmitHandler}>
+          <input
+            type="text"
+            value={userInput}
+            onChange={(e) => setuserInput(e.target.value)}
+            placeholder={"Please start typing something..."}
+          />
+        </form>
+      </div>
     </div>
   );
 };
